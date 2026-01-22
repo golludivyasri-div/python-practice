1c7b0197f2b36cd1f2f9e5446becd2ebb6053551
golludivyasri-div <golludivyasri@gmail.com>
2026-01-22 13:57:46 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,16 @@
+#print("hellow world")
+#print("My name is Divya sri")
+#VARIABLES
+#name= "Divya sri"
+#college="IIIT ongole"
+#print("my name is",name)
+#print("my college is",college)
+#name="divya sri"
+#branch="ECE"
+#print("my name is",name)
+#print("branch:",branch)
+#NUMBERS+SIMPLE CALCULATION
+#a=int(input("enter first number:"))
+#b=int(input("enter second number:"))
+#sum=a+b
+#print("sum is:",sum)
